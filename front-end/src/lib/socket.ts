908bfdf6ef7475b2c57fd24bed8878968b60f921
Zahidul Hossain <choyan.009@gmail.com>
2025-01-11 14:07:47 +0600

    fix: front-end websocket url updated

--- front-end/src/lib/socket.ts
+++ front-end/src/lib/socket.ts
@@ -1,8 +1,6 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "@/config";
 
-const URL = process.env.NODE_ENV === "production" ? undefined : BASE_URL;
-
-export const socket = io(URL, {
+export const socket = io(BASE_URL, {
   autoConnect: true,
 });
